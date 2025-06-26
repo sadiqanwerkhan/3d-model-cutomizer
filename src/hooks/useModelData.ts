993bba94a9d3e405b59d2bdaee6d5d3984481704
Muhammad Sadiq Anwer Khan <Muhammad Sadiq Anwer Khan>
2025-06-26 05:18:39 +0200

    Loaded and rendered 3D model dynamically from external API using React Three Fiber

--- src/hooks/useModelData.ts
+++ src/hooks/useModelData.ts
@@ -1,1 +1,14 @@
-// TODO: Will use React Query to fetch model.json from MODEL_API_URL
+import { useQuery } from "@tanstack/react-query";
+import { MODEL_API_URL } from "../constants/api";
+import type { ModelApiResponse } from "../types/model";
+
+export const useModelData = () => {
+  return useQuery<ModelApiResponse>({
+    queryKey: ["modelData"],
+    queryFn: async () => {
+      const res = await fetch(MODEL_API_URL);
+      if (!res.ok) throw new Error("Failed to fetch model data");
+      return res.json();
+    },
+  });
+};
